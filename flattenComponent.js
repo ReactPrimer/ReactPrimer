36267f8258c7411c35f97e08379ca2f7b6fdda86
Vince Vuong <vuong.vincent@gmail.com>
2017-11-02 16:35:38 -0700

    Add Comment

--- flattenComponent.js
+++ flattenComponent.js
@@ -1,15 +1,16 @@
+// functionality to flatten component tree data structure.
+
 function flattenComponent(components, flattened = []) {
-  components.forEach((element,index) => {
+  components.forEach((element, index) => {
     let obj = {};
     obj['title'] = element.title;
     obj['children'] = element.children;
     flattened.push(obj);
-    flattenComponent(components[index].children,flattened);
+    flattenComponent(components[index].children, flattened);
   })
   return flattened;
-}
-// console.log(flattenComponent([{title:'a',children:[{title:'b',children:[{title:'c',children:[]}]}]},{title:'d', children:[{title:'e',children:[]}]}]))
 
-module.exports = flattenComponent;
+
+  module.exports = flattenComponent;
 
 
