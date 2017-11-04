0a8771d67a1d5304dea921e6e97e31f0983785b4
Vince Vuong <vuong.vincent@gmail.com>
2017-11-04 11:50:04 -0700

    remove duplicate import statements for fileContent

--- fileContent.js
+++ fileContent.js
@@ -1,28 +1,30 @@
 //this function will return the component template to be populated in the project folder
 function fileContent(component) {
   let content = "";
+  const cache = {};
 
-  content +="import React, { Component } from 'react';\n";
+  content += "import React, { Component } from 'react';\n";
 
-  for (let i = 0; i < component.children.length; i++)
-    {
-      content+=`import ${component.children[i].title} from './${component.children[i].title}.jsx'\n`;
+  for (let i = 0; i < component.children.length; i++) {
+    if (!cache[component.children[i]]) {
+      cache[component.children[i]] = true;
+      content += `import ${component.children[i].title} from './${component.children[i].title}.jsx'\n`;
     }
+  }
 
-    content+=`\nclass ${component.title} extends Component {\n`;
-    content+=`  render() {\n`;
-    content+=`    return (\n`;
-    content+=`      <div>\n`;
+  content += `\nclass ${component.title} extends Component {\n`;
+  content += `  render() {\n`;
+  content += `    return (\n`;
+  content += `      <div>\n`;
 
-    for (let i = 0; i < component.children.length; i++)
-    {
-    content+=`        <${component.children[i].title} />\n`;
-    }
-    content+=`      </div>\n`;
-    content+=`    );\n`;
-    content+=`  };\n`;
-    content+=`}\n`;
-    content+=`export default ${component.title};`;
+  for (let i = 0; i < component.children.length; i++) {
+    content += `        <${component.children[i].title} />\n`;
+  }
+  content += `      </div>\n`;
+  content += `    );\n`;
+  content += `  };\n`;
+  content += `}\n`;
+  content += `export default ${component.title};`;
 
   return content;
 }
