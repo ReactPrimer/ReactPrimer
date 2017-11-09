d38a54e1d86f4b55e3a2be7c36fd457e570206bd
Vince Vuong <vuong.vincent@gmail.com>
2017-11-08 20:32:54 -0800

    webpack add for bundle -> to build release app

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    watch: true,
+    target: 'electron',
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/build',
+        publicPath: 'build/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader'
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                query: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'bundle.css',
+            disable: false,
+            allChunks: true
+        }
+    )
+]
+
+}
