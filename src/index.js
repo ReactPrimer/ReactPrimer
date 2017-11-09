49a7e09ee5f7af3e0a1c12a50f4cf8167b13b493
Vince Vuong <vuong.vincent@gmail.com>
2017-11-08 20:31:38 -0800

    whatever

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './src/App.js';
+import App from './App.js';
 
 ReactDom.render(<App />,document.getElementById('root'))
