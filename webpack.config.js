0a6150778ee7759aa8f194c57ef58051182d4e6a
Christian Varisco <vari.chri1993@gmail.com>
2017-12-08 13:03:03 +0100

    Add stage-2 presets to dev env

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react',"es2015"]
+                    presets: ['react',"es2015", "stage-2"]
                 }
             },
             {
